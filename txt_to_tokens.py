1bfa19bff57428ee1cfbabf0fb8ee71a1535bbc6
Wojciech C <wojtekcieplichiewicz@gmail.com>
2025-11-30 14:58:41 +0100

    text_to_tokens

---
+++ txt_to_tokens.py
@@ -0,0 +1,47 @@
+import numpy as np
+from tokenizer import CustomTokenizer
+from tqdm import tqdm
+
+tokenizer = CustomTokenizer()
+
+
+
+in_path = ".txt"
+out_file = ".bin"
+
+
+
+
+BATCH_LINES = 20000           
+total_tokens = 0
+batch = []
+
+lines=0
+with open(in_path, "r", encoding="utf-8") as f, open(out_file, "wb") as out:
+
+    for line in tqdm(f, desc="Lines", unit="lines"):
+        lines+=1
+        batch.append(line)
+        if len(batch) >= BATCH_LINES:
+            enc = tokenizer.encode(batch)   # fast batch tokenization
+            for ids in enc:
+                total_tokens += len(ids)
+                if len(ids) > 0:
+                    np.array(ids, dtype=np.int32).tofile(out)
+            batch = []
+        
+
+
+    if batch:
+        enc = tokenizer.encode(batch) 
+        for ids in enc:
+            total_tokens += len(ids)
+            if len(ids) > 0:
+                np.array(ids, dtype=np.int32).tofile(out)
+
+
+print(f"Number of lines: {lines}")
+print(f"Number of tokens: {total_tokens}")
+
+
+
